c98ed613477d954fe7c031dd236d8a68ebaa6eb6
Jonnychipz <john@luinn-ltd.co.uk>
2021-01-07 15:51:16 +0000

    update

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ terraform {
     resource_group_name   = "jonnychipz-infra"
     storage_account_name  = "jonnychipztstate"
     container_name        = "tstate"
-    key                   = "+uRmL73LSnXvSEGMG9pd26R28qvgFS9z3BcZrg+NHAExrs9HkRhOjNGWxT9c/J0iDcwF2t+txjCf0ZfDayV6pw=="
-  }
+}
 
   required_providers {
     azurerm = {
