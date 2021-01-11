746854e8341a156f06611ad0de797c7b7ded1a6a
Jonnychipz <john@luinn-ltd.co.uk>
2021-01-11 12:20:30 +0000

    key mod

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name   = "jonnychipz-infra"
     storage_account_name  = "jonnychipztstate"
     container_name        = "tstate"
-    key                   = "+uRmL73LSnXvSEGMG9pd26R28qvgFS9z3BcZrg+NHAExrs9HkRhOjNGWxT9c/J0iDcwF2t+txjCf0ZfDayV6pw=="
+    key                   = "+uRmL73LSnXvSEGMG9pd26R28qvgFS9z3BcZrg+NHAExrs9HkRhOjNGWxT9c"
 }
 
   required_providers {
