104456216b97e22f7fdb692e8e49f70b82389697
Jonnychipz <john@luinn-ltd.co.uk>
2021-01-12 13:07:54 +0000

    modify tag

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_storage_account" "jonnychipzsa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "jonnychipz"
+    environment = "jonnychipzenv"
   }
 }
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
