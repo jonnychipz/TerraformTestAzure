75d6375af37e286bc93a2aa2417f34920f406367
Jonnychipz <john@luinn-ltd.co.uk>
2021-01-12 12:29:52 +0000

    update tag

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-terraform {   
+terraform {
   required_providers {
     azurerm = {
+      # Specify what version of the provider we are going to utilise
       source = "hashicorp/azurerm"
       version = ">= 2.4.1"
     }
   }
 }
-
 provider "azurerm" {
   features {
       key_vault {
@@ -14,15 +14,12 @@ provider "azurerm" {
     }
   }
 }
-
 data "azurerm_client_config" "current" {}
-
 # Create our Resource Group - Jonnychipz-RG
 resource "azurerm_resource_group" "rg" {
   name     = "jonnychipz-app01"
   location = "UK South"
 }
-
 # Create our Virtual Network - Jonnychipz-VNET
 resource "azurerm_virtual_network" "vnet" {
   name                = "jonnychipzvnet"
@@ -30,7 +27,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
-
 # Create our Subnet to hold our VM - Virtual Machines
 resource "azurerm_subnet" "sn" {
   name                 = "VM"
@@ -38,7 +34,6 @@ resource "azurerm_subnet" "sn" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.1.0/24"]
 }
-
 # Create our Azure Storage Account - jonnychipzsa
 resource "azurerm_storage_account" "jonnychipzsa" {
   name                     = "jonnychipzsa"
@@ -50,7 +45,6 @@ resource "azurerm_storage_account" "jonnychipzsa" {
     environment = "jonnychipzenv"
   }
 }
-
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
 resource "azurerm_network_interface" "vmnic" {
   name                = "jonnychipzvm01nic"
@@ -63,7 +57,6 @@ resource "azurerm_network_interface" "vmnic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-
 # Create our Virtual Machine - Jonnychipz-VM01
 resource "azurerm_virtual_machine" "jonnychipzvm01" {
   name                  = "jonnychipzvm01"
@@ -71,27 +64,23 @@ resource "azurerm_virtual_machine" "jonnychipzvm01" {
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.vmnic.id]
   vm_size               = "Standard_B2s"
-
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2016-Datacenter-Server-Core-smalldisk"
     version   = "latest"
   }
-
   storage_os_disk {
     name              = "jonnychipzvm01os"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
     computer_name      = "jonnychipzvm01"
     admin_username     = "jonnychipz"
     admin_password     = "Password123$"
   }
-
   os_profile_windows_config {
   }
 }
