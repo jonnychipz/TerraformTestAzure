c30792c51070daec6791977af51e4ea7641075f8
manojks007 <38854704+manojks007@users.noreply.github.com>
2022-02-16 12:49:49 +0530

    deleted

--- TerraformBasics/output.tf
+++
@@ -1,9 +0,0 @@
-output "primary_location" {
-    value = azurerm_resource_group.myrg.location
-    description = "Primary Location"
-}
-
-output "vnet_address_space" {
-    value = azurerm_virtual_network.myvnet.address_space
-    description = "VNET Address Space"
-}
