0b6c317dc84d0dd44774bb08327c6eb32a939a29
manojks007 <38854704+manojks007@users.noreply.github.com>
2022-02-16 12:39:40 +0530

    mks

--- backend.sh
+++ backend.sh
@@ -1,6 +1,6 @@
 backend "azurerm" {
-    resource_group_name   = "jonnychipz-infra"
-    storage_account_name  = "jonnychipztstate"
+    resource_group_name   = "mks-infra"
+    storage_account_name  = "mkststate"
     container_name        = "tstate"
-    key                   = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
+    key                   = "xxxx"
 }
